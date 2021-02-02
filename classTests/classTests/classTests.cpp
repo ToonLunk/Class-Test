607fe5bec9142f61164bd6f3e36385f9ff11c282
ToonLunk <tylerchasebrock@gmail.com>
2021-02-02 15:18:00 -0600

    added some stuff in a class

--- classTests/classTests/classTests.cpp
+++ classTests/classTests/classTests.cpp
@@ -2,9 +2,28 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class People
+{
+private:
+	string fName; // for first name
+	string lName; // for last name
+	int age; // for age
+
+public:
+	void setFName(string);
+	void setLName(string);
+	int setAge(int);
+
+	// "string" or "int" before definition shows what the return type will be
+	string getFName() const; // const means it will not alter data
+	string getLName() const;
+	int getAge() const;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Hello World!\n";
 }
 
